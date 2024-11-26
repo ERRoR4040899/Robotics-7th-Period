533fe46a2df53f5e9dbbd5980296363ac7faac26
ERRoR4040899 <JaheemRobinson100@gmail.com>
2024-11-26 13:20:45 -0500

    Calculator
    Python Calculator

---
+++ calculator.py
@@ -0,0 +1,22 @@
+x=5
+y=9
+
+addition = x+y
+
+subtraction = x-y
+
+multiplication = x * y
+
+division = x / y
+
+power = x**y
+
+print(power)
+
+print(division)
+
+print(addition)
+
+print(subtraction)
+
+print(multiplication)
